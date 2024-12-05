a3fe2f3521ddb12671eed29d1383eaf89da31501
frank <frank@pixeldigita.com>
2024-12-05 11:07:28 -0600

    add module auth to finction-param  module

--- src/modules/function-param/function-param.module.ts
+++ src/modules/function-param/function-param.module.ts
@@ -4,9 +4,10 @@ import { FunctionParamController } from '../../controllers/function-param/functi
 import { FunctionParamService } from '../../services/function-param/function-param.service';
 import { Funcion } from '@models/agent/Function.entity';
 import { FunctionModule } from '@modules/Function/function.module';
+import { AuthModule } from '@modules/auth/auth.module';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([Funcion]), FunctionModule],
+  imports: [TypeOrmModule.forFeature([Funcion]), FunctionModule, AuthModule],
   controllers: [FunctionParamController],
   providers: [FunctionParamService],
   exports: [FunctionParamService],
